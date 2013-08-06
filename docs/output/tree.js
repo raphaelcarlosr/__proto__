1e398944cef6d7a7418c18a6327ade15188a8bf1
Raphael Carlos Rego <raphaelcarlosr@gmail.com>
2013-08-06 19:24:00 -0300

    Documentation

---
+++ docs/output/tree.js
@@ -0,0 +1,19 @@
+
+		Docs.classData ={"id":"apidocs","iconCls":"icon-docs","text":"API Documentation","singleClickExpand":true,"children":[
+                {"id":"pkg-__proto__","text":"__proto__","iconCls":"icon-pkg","cls":"package","singleClickExpand":true, children:[
+                {"href":"output/Array.html","text":"Array","id":"Array","isClass":true,"iconCls":"icon-static","cls":"cls","leaf":true}
+				,
+                {"href":"output/Object.html","text":"Object","id":"Object","isClass":true,"iconCls":"icon-static","cls":"cls","leaf":true}
+				,
+                {"href":"output/String.html","text":"String","id":"String","isClass":true,"iconCls":"icon-static","cls":"cls","leaf":true}
+				]}
+				]};
+        Docs.icons = {
+        
+			"Array":"icon-static"
+			,
+			"Object":"icon-static"
+			,
+			"String":"icon-static"
+			};
+    
